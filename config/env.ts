58ac6400b79487b6aaf5475863e8a93eb7f430bc
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-10-29 01:21:46 -0500

    Add env config

---
+++ config/env.ts
@@ -0,0 +1,11 @@
+interface Settings {
+  CLIENT_ID: string | undefined
+  CLIENT_SECRET: string | undefined
+  REDIRECT_URI: string | undefined
+}
+
+export const settings: Settings = {
+  CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
+  CLIENT_SECRET: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI
+}
