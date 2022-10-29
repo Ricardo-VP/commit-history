85543cfd5a724e7b90f37bf5cc090682b002376c
Ricardo Vaca <ricardo.vaca@ioet.com>
2022-10-29 17:59:41 -0500

    Use the get_user endpoint

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,37 @@
 'use client'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useQuery } from '@tanstack/react-query'
+
+import { settings } from '../../config/env'
+import { useUserStore } from '../../store/user'
+
+const { BACKEND_URI } = settings
 
 export const LoginPage = () => {
   const searchParams = useSearchParams()
-  const code = searchParams.get('code')
+  const code = searchParams.get('code') ?? ''
 
-  console.log(code)
+  const { logIn } = useUserStore()
+  const router = useRouter()
+
+  useQuery(
+    ['userData'],
+    async () => {
+      return await fetch(`${BACKEND_URI as string}/user/${code}`).then(
+        async (res) => await res.json()
+      )
+    },
+    {
+      onSuccess: (user) => {
+        if (!user.data) {
+          router.push('/')
+        } else {
+          logIn(true, user.data)
+        }
+      },
+      staleTime: Infinity
+    }
+  )
 
   return (
     <div>
--- config/env.ts
+++ config/env.ts
@@ -2,10 +2,12 @@ interface Settings {
   CLIENT_ID: string | undefined
   CLIENT_SECRET: string | undefined
   REDIRECT_URI: string | undefined
+  BACKEND_URI: string | undefined
 }
 
 export const settings: Settings = {
   CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
   CLIENT_SECRET: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-  REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI
+  REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
+  BACKEND_URI: process.env.NEXT_PUBLIC_BACKEND_URI
 }
